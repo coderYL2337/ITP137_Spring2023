1b1faff1dc2610b46989097daff66185bec12283
Yan <124451677+coderYL2337@users.noreply.github.com>
2023-02-18 16:27:24 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,107 @@
+
+//print("Hello,world!")
+//input
+import Foundation
+import Glibc
+//print()
+//how to declare if-else in swift?
+// var varA = 100;
+// if varA == 20 {
+//    print("varA is equal to than 20");
+// } else if varA == 50 {
+//    print("varA is equal to than 50");
+// } else { 
+//    print("None of the values is matching");
+// }
+// print("Value of variable varA is \(varA)");
+
+
+// print("Enter a number")
+// let number = Int(readLine()!)!
+// if number>0 {
+//   print("Number is positive")
+// }
+// else if number<0 {
+//   print("Number is negative")
+// }
+// else {
+//   print("Number is neither positive or negative")
+// }
+
+//how to declare switch statement
+// let n = 2
+// switch n {
+//   case 1: 
+//        print("It's one!")
+//   case 2...4:
+//       print("It's two to four!")
+//   case 5,6 :
+//      print("It's 5 or 6!")
+//   default:
+//      print("It's another number!")
+// }
+
+// print("Give me a number!")
+// var n = Int(readLine()!)!
+// switch n {
+//   case 1: 
+//        print("It's one!")
+//   case 2...4:
+//       print("It's two to four!")
+//   case 5,6 :
+//      print("It's 5 or 6!")
+//   default:
+//      print("It's another number!")
+// }
+
+// print("Give me a number!")
+// var n = Int(readLine()!)!
+// switch n {
+//   case -99...(-1): 
+//        print("It's negative!")
+//   case 0: 
+//        print("It's zero!")
+//   case 1...100:
+//       print("It's positive!")
+//   default:
+//      print("It's out of scope!")
+// }
+
+
+//how to declare a switch statement
+// var someCharacter = "z"
+// switch someCharacter {
+// case "a":
+//     print("The first letter of the alphabet")
+// case "z":
+//     print("The last letter of the alphabet")
+// default:
+//     print("Some other character")
+// }
+// Prints "The last letter of the alphabet"
+//how to declare a loop in swift
+//while loop
+// var i = 1
+// var sum = 0
+// while i < 10 {
+//     sum = sum + i //it’s like we did 1+2+3+4+5+6+7+8+9!
+//     i+=1
+// }
+// print(sum) //prints 45
+// // for loop
+// var sum = 0
+// for i in 1...9 {
+//     sum += i  //it’s like we did 1+2+3+4+5+6+7+8+9!
+// }
+// print(sum) //prints 45
+//repeat...while
+// repeat {
+//    statement(s);
+// } 
+// while( condition );
+//  var index = 10
+//  repeat {
+//    print( "Value of index is \(index)")
+//    index = index + 1
+// }
+// while index < 20
